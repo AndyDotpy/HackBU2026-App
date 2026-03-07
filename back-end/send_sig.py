9216f3c25e78c14eb93cc8de008ff29b77522a56
Ace-of-Spades51 <143812804+Ace-of-Spades51@users.noreply.github.com>
2026-03-07 18:49:16 -0500

    send_sig.py WIP, test function provided

---
+++ back-end/send_sig.py
@@ -0,0 +1,27 @@
+import os
+import signal
+
+def end_process(pid, sig):
+    try:
+        match sig:
+            case "SIGTERM":
+                os.kill(pid, signal.SIGTERM)
+            case "SIGKILL":
+                os.kill(pid, signal.SIGKILL)
+            case _:
+                print("Error: Invalid signal")
+                return False
+    except OSError:
+        print("Error: pid not found")
+        return False
+    else:
+        return True
+'''
+Either kills or terminates a process given a process ID and signal
+Args:
+    pid (int): the process ID
+    sig (string): the signal
+Return: True for success, False for failure
+'''
+
+end_process(33736, "SIG")
